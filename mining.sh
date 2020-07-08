a5c7decb935facca8972a40e397b8980b4b25f9a
lapakdigital <37134646+lapakdigital@users.noreply.github.com>
2020-07-08 16:05:09 +0700

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -2,6 +2,6 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz 
 while [ 1 ]; do
-./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MhaweNm6sj2PeBsxzJN5u1LChuUqKEGzEC.ibm -t6
+./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-eu.rplant.xyz:17022 -u MhaweNm6sj2PeBsxzJN5u1LChuUqKEGzEC.ibm -t18
 sleep 2
 done 
